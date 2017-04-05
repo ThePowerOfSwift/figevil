4b38e12a8ce3594ed83739a8f5399be5cfc52645
satorusasozaki <sasozakisatoru@gmail.com>
2017-04-05 16:36:13 -0700

    Add photo effect filter

--- figevil/Filter.swift
+++ figevil/Filter.swift
@@ -49,10 +49,27 @@ class Filter: NSObject {
         return [Filter(name: "Plain", filter: nil, imageUrlString: "chinatown.jpg"),
                 Filter(name: "Sepia", filter: CIFilter(name: "CISepiaTone"), imageUrlString: "golden_gate_bridge.jpg"),
                 Filter(name: "False", filter: CIFilter(name: "CIFalseColor"), imageUrlString: "chinatown.jpg"),
-                Filter(name: "Plain", filter: nil, imageUrlString: "chinatown.jpg"),
+                Filter(name: "EffectChrome", filter: photoEffectChrome, imageUrlString: "golden_gate_bridge.jpg"),
+                Filter(name: "EffectFade", filter: photoEffectFade, imageUrlString: "golden_gate_bridge.jpg"),
+                Filter(name: "EffectInstant", filter: photoEffectInstant, imageUrlString: "golden_gate_bridge.jpg"),
+                Filter(name: "EffectMono", filter: photoEffectMono, imageUrlString: "golden_gate_bridge.jpg"),
+                Filter(name: "EffectNoir", filter: photoEffectNoir, imageUrlString: "golden_gate_bridge.jpg"),
+                Filter(name: "EffectProcess", filter: photoEffectProcess, imageUrlString: "golden_gate_bridge.jpg"),
+                Filter(name: "EffectTonal", filter: photoEffectTonal, imageUrlString: "golden_gate_bridge.jpg"),
+                Filter(name: "EffectTransfer", filter: photoEffectTransfer, imageUrlString: "golden_gate_bridge.jpg"),
                 Filter(name: "Clamp", filter: colorClamp, imageUrlString: "chinatown.jpg"),
-                Filter(name: "Controls", filter: colorControls, imageUrlString: "chinatown.jpg"),
-                Filter(name: "Matrix", filter: colorMatrix, imageUrlString: "chinatown.jpg")
+//                Filter(name: "Controls", filter: colorControls, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "Matrix", filter: colorMatrix, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "Polynomial", filter: colorPolynomial, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "CrossPolynomial", filter: colorCrossPolynomial, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "Cube", filter: colorCube, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "CubeWithColorSpace", filter: colorCubeWithColorSpace, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "Invert", filter: colorInvert, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "Map", filter: colorMap, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "Monochrome", filter: colorMonochrome, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "Posterize", filter: colorPosterize, imageUrlString: "chinatown.jpg"),
+//                Filter(name: "Vignette", filter: vignetteEffect, imageUrlString: "golden_gate_bridge.jpg")
+
         ]
     }
     
@@ -68,6 +85,7 @@ class Filter: NSObject {
     
     class var colorControls: CIFilter? {
         let filter = CIFilter(name: "CIColorControls")
+        
         return filter
     }
     
@@ -76,6 +94,96 @@ class Filter: NSObject {
         return filter
     }
     
+    class var colorPolynomial: CIFilter? {
+        let filter = CIFilter(name: "CIColorPolynomial")
+        return filter
+    }
+    
+    // MARK: CICategoryColorEffect
+    
+    class var colorCrossPolynomial: CIFilter? {
+        let filter = CIFilter(name: "CIColorCrossPolynomial")
+        return filter
+    }
+    
+    class var colorCube: CIFilter? {
+        let filter = CIFilter(name: "CIColorCube")
+
+        return filter
+    }
+
+
+    
+    class var colorCubeWithColorSpace: CIFilter? {
+        let filter = CIFilter(name: "CIColorCubeWithColorSpace")
+        return filter
+    }
+    
+    class var colorInvert: CIFilter? {
+        let filter = CIFilter(name: "CIColorInvert")
+        return filter
+    }
+    
+    class var colorMap: CIFilter? {
+        let filter = CIFilter(name: "CIColorMap")
+        return filter
+    }
+    
+    class var colorMonochrome: CIFilter? {
+        let filter = CIFilter(name: "CIColorMonochrome")
+        return filter
+    }
+    
+    class var colorPosterize: CIFilter? {
+        let filter = CIFilter(name: "CIColorPosterize")
+        return filter
+    }
+    
+    class var photoEffectChrome: CIFilter? {
+        let filter = CIFilter(name: "CIPhotoEffectChrome")
+        
+        return filter
+    }
+    
+    class var photoEffectFade: CIFilter? {
+        let filter = CIFilter(name: "CIPhotoEffectFade")
+        return filter
+    }
+    
+    class var photoEffectInstant: CIFilter? {
+        let filter = CIFilter(name: "CIPhotoEffectInstant")
+        return filter
+    }
+    
+    class var photoEffectMono: CIFilter? {
+        let filter = CIFilter(name: "CIPhotoEffectMono")
+        return filter
+    }
+    
+    class var photoEffectNoir: CIFilter? {
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        return filter
+    }
+    
+    class var photoEffectProcess: CIFilter? {
+        let filter = CIFilter(name: "CIPhotoEffectProcess")
+        return filter
+    }
+    
+    class var photoEffectTonal: CIFilter? {
+        let filter = CIFilter(name: "CIPhotoEffectTonal")
+        return filter
+    }
+    
+    class var photoEffectTransfer: CIFilter? {
+        let filter = CIFilter(name: "CIPhotoEffectTransfer")
+        return filter
+    }
+    
+    class var vignetteEffect: CIFilter? {
+        let filter = CIFilter(name: "CIVignetteEffect")
+        return filter
+    }
     
     
     
